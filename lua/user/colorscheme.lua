d201ae33d0472c2c1dcbb15bed68b1699cb29c7b
Jefex7 <jefex7@Jefex7s-MacBook-Pro.local>
2023-12-24 18:09:42 +0000

    Changed color LineNr

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -10,6 +10,9 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "LineNr",{ fg = "#d8f5f1" })
+	vim.api.nvim_set_hl(0, "LineNrAbove",{ fg = "#3f5754" })
+	vim.api.nvim_set_hl(0, "LineNrBelow",{ fg = "#3f5754" })
 
 end
 
